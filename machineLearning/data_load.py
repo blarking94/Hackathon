66e1fc61a856d03933fb88b32a82b707edabd268
Ben Larking <blarking2014@gmail.com>
2018-01-18 11:35:29 +0000

    trying to load data and features

--- machineLearning/data_load.py
+++ machineLearning/data_load.py
@@ -1,17 +1,12 @@
 import os
 from six.moves.urllib.request import urlopen
+import tensorflow as tf
 
 import config
 
 # Data sets
 CAR_DATA = "car_training.csv"
 CAR_DATA_URL = config.CAR_DATA_URL
-
-#  70/30 split training and test data
-CAR_TRAINING_DATA
-
-CAR_TEST_DATA
-
 
 def main():
   # If the training and test sets aren't stored locally, download them.
@@ -21,7 +16,32 @@ def main():
       f.write(raw)
 
   # Split the data sets randomly 70 / 30
-  
+  with tf.Session() as sess:
+    sess.run( tf.global_variables_initializer())
+
+    features = tf.placeholder(tf.int32, shape=[6], name='features')
+    acceptability = tf.placeholder(tf.string, name='acceptability')
+    #total = tf.reduce_sum(features, name='total')
+
+    printerop = tf.Print(acceptability, [features], name='printer')
+
+    with open(CAR_DATA) as inf:
+        for line in inf:
+            # Read data, using python, into our features
+            print(line)
+            buying, maint, doors, persons, lug_boot, safety, acceptability = line.strip().split(",")
+
+            buying = str(buying)
+            maint = str(maint)
+            doors = str(doors)
+            persons = str(persons)
+            lug_boot = str(lug_boot)
+            safety = str(safety)
+
+            # Run the Print ob
+            print("read in data")
+            total = sess.run(printerop, feed_dict={features: [buying, maint, doors, persons, lug_boot, safety], acceptability:acceptability})
+            print(acceptability, total)
 
 
 main()
